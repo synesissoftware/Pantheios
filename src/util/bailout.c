a0b8e2f78736e793b8c18783824567c98a7eb6f7
Matt Wilson <stlsoft@gmail.com>
2021-03-21 12:38:40 +1100

    ~ preparatory changes

--- src/util/bailout.c
+++ src/util/bailout.c
@@ -4,10 +4,11 @@
  * Purpose:     Implementation file for low-level Pantheios bail out.
  *
  * Created:     21st June 2005
- * Updated:     18th October 2019
+ * Updated:     19th January 2021
  *
  * Home:        http://www.pantheios.org/
  *
+ * Copyright (c) 2019-2021, Matthew Wilson and Synesis Information Systems
  * Copyright (c) 2005-2019, Matthew Wilson and Synesis Software
  * Copyright (c) 1999-2005, Synesis Software and Matthew Wilson
  * All rights reserved.
@@ -21,9 +22,10 @@
  * - Redistributions in binary form must reproduce the above copyright
  *   notice, this list of conditions and the following disclaimer in the
  *   documentation and/or other materials provided with the distribution.
- * - Neither the name(s) of Matthew Wilson and Synesis Software nor the
- *   names of any contributors may be used to endorse or promote products
- *   derived from this software without specific prior written permission.
+ * - Neither the name(s) of Matthew Wilson and Synesis Information Systems
+ *   nor the names of any contributors may be used to endorse or promote
+ *   products derived from this software without specific prior written
+ *   permission.
  *
  * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS
  * IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
@@ -75,7 +77,8 @@
 #include <stdarg.h>
 #include <stdio.h>
 #include <string.h>
-#if defined(PLATFORMSTL_OS_IS_UNIX)
+#if 0
+#elif defined(PLATFORMSTL_OS_IS_UNIX)
 # include <time.h>
 # include <sys/time.h>
 #endif /* PLATFORMSTL_OS_IS_UNIX */
@@ -85,7 +88,8 @@
 
 /* Operating-system header files */
 
-#if defined(PLATFORMSTL_OS_IS_UNIX)
+#if 0
+#elif defined(PLATFORMSTL_OS_IS_UNIX)
 # ifndef PANTHEIOS_BAILOUT_NO_UNIX_SYSLOG
 #  include <syslog.h>
 # endif /* !PANTHEIOS_BAILOUT_NO_UNIX_SYSLOG */
@@ -105,7 +109,8 @@
 # define _PANTHEIOS_COMPILER_REQUIRES_EXTERNCPP_DEFINITIONS
 #endif /* compiler */
 
-#if defined(STLSOFT_COMPILER_IS_BORLAND) || \
+#if 0
+#elif defined(STLSOFT_COMPILER_IS_BORLAND) || \
     defined(STLSOFT_COMPILER_IS_DMC) || \
     (   defined(STLSOFT_COMPILER_IS_INTEL) && \
         defined(_WIN32)) || \
@@ -167,7 +172,6 @@ namespace pantheios
  * Event Log use message strings defined within the Pantheios.COM module.
  */
 static int pantheios_util_onBailOut_canUseWarnMessage_(void);
-
 #endif /* PLATFORMSTL_OS_IS_WINDOWS && !defined(PANTHEIOS_BAILOUT_NO_WINDOWS_EVENTLOG */
 
 /* /////////////////////////////////////////////////////////////////////////
@@ -175,11 +179,11 @@ static int pantheios_util_onBailOut_canUseWarnMessage_(void);
  */
 
 #ifndef PANTHEIOS_BAILOUT_STACK_BUFFER_SIZE
-# define PANTHEIOS_BAILOUT_STACK_BUFFER_SIZE    (2048)
+# define PANTHEIOS_BAILOUT_STACK_BUFFER_SIZE                (2048)
 #endif /* !PANTHEIOS_BAILOUT_STACK_BUFFER_SIZE */
 
 #ifndef PANTHEIOS_BAILOUT_BAILOUT_FILE_NAME
-# define PANTHEIOS_BAILOUT_BAILOUT_FILE_NAME    "logging-bailout.txt"
+# define PANTHEIOS_BAILOUT_BAILOUT_FILE_NAME                "logging-bailout.txt"
 #endif /* !PANTHEIOS_BAILOUT_BAILOUT_FILE_NAME */
 
 #if defined(PLATFORMSTL_OS_IS_WINDOWS) && \
@@ -189,15 +193,15 @@ static int pantheios_util_onBailOut_canUseWarnMessage_(void);
  * *not* be changed).
  */
 
-# define BAILOUT_1PARAM_FAIL_MESSAGE_ID         ((DWORD)1000)
-# define BAILOUT_2PARAM_FAIL_MESSAGE_ID         ((DWORD)1001)
+# define BAILOUT_1PARAM_FAIL_MESSAGE_ID                     ((DWORD)1000)
+# define BAILOUT_2PARAM_FAIL_MESSAGE_ID                     ((DWORD)1001)
 
 /* The following two are 'known' from pantheios.COM, v1.0.1-b31+. (They will
  * *not* be changed).
  */
 
-# define BAILOUT_1PARAM_WARN_MESSAGE_ID         ((DWORD)1005)
-# define BAILOUT_2PARAM_WARN_MESSAGE_ID         ((DWORD)1006)
+# define BAILOUT_1PARAM_WARN_MESSAGE_ID                     ((DWORD)1005)
+# define BAILOUT_2PARAM_WARN_MESSAGE_ID                     ((DWORD)1006)
 
 #endif /* PLATFORMSTL_OS_IS_WINDOWS && !defined(PANTHEIOS_BAILOUT_NO_WINDOWS_EVENTLOG */
 
@@ -205,7 +209,8 @@ static int pantheios_util_onBailOut_canUseWarnMessage_(void);
  * util API
  */
 
-PANTHEIOS_CALL(void) pantheios_onBailOut6(
+PANTHEIOS_CALL(void)
+pantheios_onBailOut6(
     int         severity
 ,   char const* message
 ,   char const* processId
@@ -214,7 +219,7 @@ PANTHEIOS_CALL(void) pantheios_onBailOut6(
 ,   char const* beName
 )
 {
-    if( NULL == feName &&
+    if (NULL == feName &&
         NULL == beName)
     {
         pantheios_onBailOut4(severity, message, processId, qualifier);
@@ -234,92 +239,92 @@ PANTHEIOS_CALL(void) pantheios_onBailOut6(
         memset(&qualifier_[0], '~', sizeof(qualifier_));
 #endif
 
-        if(0 != cchFeName)
+        if (0 != cchFeName)
         {
             int r = 0;
 
-            if(p1 < end)
+            if (p1 < end)
             {
                 p1[0] = '\0';
 
                 r = pantheios_util_snprintf_a(p1, (end - p1), "fe=%.*s", (int)cchFeName, feName);
             }
 
-            if(r < 0)
+            if (r < 0)
             {
                 /* Some runtime libraries' snprintf() return -ve if write to end */
-                if('\0' != p1[0])
+                if ('\0' != p1[0])
                 {
                     r = 3 + stlsoft_static_cast(int, cchFeName);
                 }
             }
 
-            if(r > (end - p1))
+            if (r > (end - p1))
             {
                 p1 = end;
             }
             else
-            if(r > 0)
+            if (r > 0)
             {
                 p1 += r;
             }
         }
-        if(0 != cchBeName)
+        if (0 != cchBeName)
         {
             int r = 0;
 
-            if(p1 < end)
+            if (p1 < end)
             {
                 p1[0] = '\0';
 
                 r = pantheios_util_snprintf_a(p1, (end - p1), &", be=%.*s"[(0 != cchFeName) ? 0 : 2], (int)cchBeName, beName);
             }
 
-            if(r < 0)
+            if (r < 0)
             {
                 /* Some runtime libraries' snprintf() return -ve if write to end */
-                if('\0' != p1[0])
+                if ('\0' != p1[0])
                 {
                     r = ((0 != cchFeName) ? 0 : 2) + 3 + stlsoft_static_cast(int, cchFeName);
                 }
             }
 
-            if(r > (end - p1))
+            if (r > (end - p1))
             {
                 p1 = end;
             }
             else
-            if(r > 0)
+            if (r > 0)
             {
                 p1 += r;
             }
         }
-        if(0 != cchQualifier)
+        if (0 != cchQualifier)
         {
             int r = 0;
 
-            if(p1 < end)
+            if (p1 < end)
             {
                 p1[0] = '\0';
 
                 r = pantheios_util_snprintf_a(p1, (end - p1), ": %.*s", (int)cchQualifier, qualifier);
             }
 
-            if(r < 0)
+            if (r < 0)
             {
                 /* Some runtime libraries' snprintf() return -ve if write to end */
-                if('\0' != p1[0])
+                if ('\0' != p1[0])
                 {
                     r = 2 + stlsoft_static_cast(int, cchFeName);
                 }
             }
 
-            if(r > (end - p1))
+            if (r > (end - p1))
             {
                 p1 = end;
             }
             else
-            if(r > 0)
+            if (r > 0)
             {
                 p1 += r;
             }
@@ -333,14 +338,15 @@ PANTHEIOS_CALL(void) pantheios_onBailOut6(
     }
 }
 
-PANTHEIOS_CALL(void) pantheios_onBailOut4(
+PANTHEIOS_CALL(void)
+pantheios_onBailOut4(
     int         severity
 ,   char const* message
 ,   char const* processId
 ,   char const* qualifier
 )
 {
-    if(NULL == qualifier)
+    if (NULL == qualifier)
     {
         pantheios_onBailOut3(severity, message, processId);
 
@@ -350,13 +356,13 @@ PANTHEIOS_CALL(void) pantheios_onBailOut4(
     {
         size_t cchQualifier = pan_strlen_m_(qualifier);
 
-        if(NULL == message)
+        if (NULL == message)
         {
             pantheios_onBailOut3(severity, qualifier, processId);
 
             return;
         }
-        else if(0 == cchQualifier)
+        else if (0 == cchQualifier)
         {
             pantheios_onBailOut3(severity, message, processId);
 
@@ -369,7 +375,7 @@ PANTHEIOS_CALL(void) pantheios_onBailOut4(
             size_t          cchMessage  =   pan_strlen_m_(message);
             const char      sep[]       =   { ':', ' ', '\0' }; /* ": " */
 
-            if(cchBuf < (cchMessage + 2 + cchQualifier))
+            if (cchBuf < (cchMessage + 2 + cchQualifier))
             {
                 /* Won't fit, so need to shrink message or qualifier or both
                  *
@@ -378,19 +384,19 @@ PANTHEIOS_CALL(void) pantheios_onBailOut4(
                  * - if both > buffer, then make each half buffer
                  */
 
-                if( cchQualifier < (cchBuf - 2) / 3 &&
+                if (cchQualifier < (cchBuf - 2) / 3 &&
                     cchMessage > (cchBuf - 2) - cchQualifier)
                 {
                     cchMessage = (cchBuf - 2) - cchQualifier;
                 }
                 else
-                if( cchMessage < (cchBuf - 2) * 2 / 3 &&
+                if (cchMessage < (cchBuf - 2) * 2 / 3 &&
                     cchQualifier > (cchBuf - 2) - cchMessage)
                 {
                     cchQualifier = (cchBuf - 2) - cchMessage;
                 }
                 else
-                if( cchMessage > (cchBuf - 2) &&
+                if (cchMessage > (cchBuf - 2) &&
                     cchQualifier > (cchBuf - 2))
                 {
                     cchMessage = (cchBuf - 2) / 2;
@@ -403,7 +409,7 @@ PANTHEIOS_CALL(void) pantheios_onBailOut4(
                      * do it in a coarse-grained manner: shrink the bigger
                      * one down to size
                      */
-                    if(cchMessage > cchQualifier)
+                    if (cchMessage > cchQualifier)
                     {
                         PANTHEIOS_CONTRACT_ENFORCE_ASSUMPTION(cchQualifier < (cchBuf - 2));
 
@@ -427,7 +433,7 @@ PANTHEIOS_CALL(void) pantheios_onBailOut4(
             PANTHEIOS_CONTRACT_ENFORCE_ASSUMPTION(cchMessage + 2 + cchQualifier <= cchBuf);
 
             memcpy(&message_[0], message, cchMessage * sizeof(char));
-            if(0 == cchQualifier)
+            if (0 == cchQualifier)
             {
                 message_[cchMessage] = '\0';
             }
@@ -444,7 +450,8 @@ PANTHEIOS_CALL(void) pantheios_onBailOut4(
     }
 }
 
-PANTHEIOS_CALL(void) pantheios_onBailOut3(
+PANTHEIOS_CALL(void)
+pantheios_onBailOut3(
     int         severity
 ,   char const* message
 ,   char const* processId
@@ -473,14 +480,15 @@ PANTHEIOS_CALL(void) pantheios_onBailOut3(
 
     severity &= 0x7;    /* Bailout ignores any custom severity information. */
 
-    if(NULL == message)
+    if (NULL == message)
     {
         message = "Unspecified failure";
     }
 
     cchMessage = pan_strlen_m_(message);
 
-#if defined(PLATFORMSTL_OS_IS_WINDOWS)
+#if 0
+#elif defined(PLATFORMSTL_OS_IS_WINDOWS)
 
     GetLocalTime(&st);
 
@@ -498,7 +506,6 @@ PANTHEIOS_CALL(void) pantheios_onBailOut3(
                             ,   st.wSecond
                             ,   st.wMilliseconds
                             );
-
 #else /* ? OS */
 
     gettimeofday(&tv, NULL);
@@ -524,7 +531,7 @@ PANTHEIOS_CALL(void) pantheios_onBailOut3(
 
     PANTHEIOS_CONTRACT_ENFORCE_POSTCONDITION_RETURN_INTERNAL((cchTime < STLSOFT_NUM_ELEMENTS(message_) - 3), "time conversion overwrote the local buffer capacity");
 
-    if((cchTime + cchMessage) < (STLSOFT_NUM_ELEMENTS(message_) - 3))
+    if ((cchTime + cchMessage) < (STLSOFT_NUM_ELEMENTS(message_) - 3))
     {
         cchTotal = cchTime + cchMessage;
     }
@@ -544,9 +551,11 @@ PANTHEIOS_CALL(void) pantheios_onBailOut3(
     message_[cchTotal + 1] = '\n';
     message_[cchTotal + 2] = '\0';
 
-#if defined(PLATFORMSTL_OS_IS_WINDOWS)
+#if 0
+#elif defined(PLATFORMSTL_OS_IS_WINDOWS)
+
     /* /////////////////////////////////
-     * 1. Debugger
+     * 1. Debugger [Windows-only]
      */
 
     pan_OutputDebugString_m_(message_);
@@ -583,7 +592,7 @@ PANTHEIOS_CALL(void) pantheios_onBailOut3(
     }
 
     /* /////////////////////////////////
-     * 4. Windows Event Log
+     * 4. Event Log [Windows-only]
      *
      * This uses a special event source registered by
      * Pantheios.COM. If that's not been installed, it
@@ -594,13 +603,13 @@ PANTHEIOS_CALL(void) pantheios_onBailOut3(
 
     hEventSrc = RegisterEventSourceA(NULL, "logging-bailout");
 
-    if(NULL != hEventSrc)
+    if (NULL != hEventSrc)
     {
         char const* strings[2];
         WORD        type;
         DWORD       eventId;
 
-        if( severity < PANTHEIOS_SEV_WARNING ||
+        if (severity < PANTHEIOS_SEV_WARNING ||
             !pantheios_util_onBailOut_canUseWarnMessage_())
         {
             eventId = (NULL != processId) ? BAILOUT_2PARAM_FAIL_MESSAGE_ID : BAILOUT_1PARAM_FAIL_MESSAGE_ID;
@@ -632,23 +641,23 @@ PANTHEIOS_CALL(void) pantheios_onBailOut3(
         GetLastError();
     }
 # else /* ? PANTHEIOS_BAILOUT_NO_WINDOWS_EVENTLOG */
+
     STLSOFT_SUPPRESS_UNUSED(severity);
     STLSOFT_SUPPRESS_UNUSED(processId);
 # endif /* PANTHEIOS_BAILOUT_NO_WINDOWS_EVENTLOG */
-
 #else /* ? OS */
 
     STLSOFT_SUPPRESS_UNUSED(processId);
 
     /* /////////////////////////////////
-     * 1. Console
+     * 2. Console
      */
 
     /* NOTE: will not be valid if widestring */
     fprintf(stderr, "%.*s\n", (int)cchTotal, message_);
 
     /* /////////////////////////////////
-     * 2. File
+     * 3. File
      */
 
 # ifdef PANTHEIOS_USING_SAFE_STR_FUNCTIONS
@@ -694,18 +703,18 @@ static int pantheios_util_onBailOut_canUseWarnMessage_x_(void)
     int     canUseWarnMessage = 0;
     HKEY    key;
 
-    if(0 == RegOpenKeyA(HKEY_LOCAL_MACHINE, "SOFTWARE\\Synesis Software\\Logging Tools\\Pantheios\\Pantheios.COM", &key))
+    if (0 == RegOpenKeyA(HKEY_LOCAL_MACHINE, "SOFTWARE\\Synesis Software\\Logging Tools\\Pantheios\\Pantheios.COM", &key))
     {
         DWORD   actualVer;
         DWORD   cbData  =   sizeof(actualVer);
 
-        if(0 == RegQueryValueExA(key, "Version", NULL, NULL, stlsoft_reinterpret_cast(LPBYTE, &actualVer), &cbData))
+        if (0 == RegQueryValueExA(key, "Version", NULL, NULL, stlsoft_reinterpret_cast(LPBYTE, &actualVer), &cbData))
         {
             /* Check that version >= 1.0.1b31 */
 
             DWORD const requiredVer = 0x0100011f;
 
-            if(actualVer >= requiredVer)
+            if (actualVer >= requiredVer)
             {
                 canUseWarnMessage = 1;
             }
@@ -724,7 +733,7 @@ static int pantheios_util_onBailOut_canUseWarnMessage_(void)
 
     static int      s_canUseWarnMessage =   0;
 
-    if( 0 == s_prevTickCount ||
+    if (0 == s_prevTickCount ||
         0 == tickCount ||
         tickCount < s_prevTickCount ||
         (tickCount - s_prevTickCount) > (5 * 60 * 1000))
@@ -735,7 +744,6 @@ static int pantheios_util_onBailOut_canUseWarnMessage_(void)
 
     return s_canUseWarnMessage;
 }
-
 #endif /* PLATFORMSTL_OS_IS_WINDOWS && !defined(PANTHEIOS_BAILOUT_NO_WINDOWS_EVENTLOG */
 
 /* /////////////////////////////////////////////////////////////////////////
@@ -747,3 +755,4 @@ static int pantheios_util_onBailOut_canUseWarnMessage_(void)
 #endif /* !PANTHEIOS_NO_NAMESPACE */
 
 /* ///////////////////////////// end of file //////////////////////////// */
+
