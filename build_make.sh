97eb3880421a8699e3f50c97c32bfc2cd1f9c7ef
Matt <matthew@synesis.com.au>
2021-04-25 16:55:46 +1000

    build_make.sh : ~ improved logic

--- build_make.sh
+++ build_make.sh
@@ -4,19 +4,17 @@ Dir=$(cd $(dirname "$0"); pwd)
 Basename=$(basename "$0")
 CMakePath=$Dir/_build
 
-mkdir -p $CMakePath
+mkdir -p $CMakePath || exit 1
 
 cd $CMakePath
 
-
-
-
+cmake .. || exit 1
 
 echo "Executing make"
 
 make
 
-cd ..
+cd -
 
 ls -al $CMakePath
 
